007e3b9b23941b48f9257433751fd5b615c2943e
Tony Hillerson <tony.hillerson@gmail.com>
2024-08-30 14:15:03 -0600

    Initalize Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.3"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.thcontest"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven {
+		url= uri("https://artifacts.unidata.ucar.edu/repository/unidata-all/")
+	}
+}
+
+dependencies {
+	implementation("edu.ucar:cdm-core:5.6.0")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
