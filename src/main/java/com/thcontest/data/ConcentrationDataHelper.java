967a85c2d7611055b45fca3c513af30e46e5b14d
Tony Hillerson <tony.hillerson@gmail.com>
2024-08-31 23:15:24 -0600

    Refactoring concentration data helper for reuse

--- src/main/java/com/thcontest/data/ConcentrationDataHelper.java
+++ src/main/java/com/thcontest/data/ConcentrationDataHelper.java
@@ -6,24 +6,31 @@ import com.thcontest.response.NetcdfFileDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ucar.ma2.Array;
-import ucar.ma2.IndexIterator;
 import ucar.ma2.InvalidRangeException;
 import ucar.nc2.Dimension;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFiles;
 import ucar.nc2.Variable;
-import ucar.nc2.write.Ncdump;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ConcentrationDataHelper {
   private final Logger logger = LoggerFactory.getLogger(ConcentrationDataHelper.class);
   private URI concentrationFileURI;
+
+  private static class ConcentrationVarInfo {
+    int xDimensionIndex;
+    int yDimensionIndex;
+    Dimension xDimension;
+    Dimension yDimension;
+    Array array;
+
+  }
 
   private ConcentrationDataHelper() { /* use a static factory method */ }
 
@@ -54,31 +61,20 @@ public class ConcentrationDataHelper {
 
   public ConcentrationData concentrationAtTimeAndZ(Integer timeIndex, Integer zIndex) throws IOException, InvalidRangeException {
     try (NetcdfFile ncfile = NetcdfFiles.openInMemory(concentrationFileURI)) {
-      // double concentration(time=8, z=1, y=27, x=36);
-      Variable v = ncfile.findVariable("concentration");
-
-      int xDimensionIndex = v.findDimensionIndex("x");
-      int yDimensionIndex = v.findDimensionIndex("y");
-      Dimension xDimension = v.getDimension(xDimensionIndex);
-      Dimension yDimension = v.getDimension(yDimensionIndex);
-      int[] origin = new int[]{timeIndex, zIndex, 0, 0};
-      int[] size = new int[]{1, 1, yDimension.getLength(), xDimension.getLength()};
-
-      String[][] concentrationField = new String[yDimension.getLength()][xDimension.getLength()];
-
-      Array a = v.read(origin, size);
+      var concentrationInfo = infoForTimeAndZ(timeIndex, zIndex, ncfile);
+      var concentrationField = new String[concentrationInfo.yDimension.getLength()][concentrationInfo.xDimension.getLength()];
       List<ConcentrationDataPoint> concentrations = new ArrayList<>();
 
-      IndexIterator iter = a.getIndexIterator();
-      int currentX, currentY;
       double currentConcentration;
       String formattedConcentration;
+      int currentX, currentY;
       int[] currentCounter;
+      var iter = concentrationInfo.array.getIndexIterator();
       while (iter.hasNext()) {
         currentConcentration = iter.getDoubleNext();
         currentCounter = iter.getCurrentCounter();
-        currentX = currentCounter[xDimensionIndex];
-        currentY = currentCounter[yDimensionIndex];
+        currentX = currentCounter[concentrationInfo.xDimensionIndex];
+        currentY = currentCounter[concentrationInfo.yDimensionIndex];
         if (currentConcentration == 0.0) {
           formattedConcentration = "0.0";
         } else {
@@ -90,5 +86,30 @@ public class ConcentrationDataHelper {
 
       return new ConcentrationData(timeIndex, zIndex, concentrationField, concentrations);
     }
+  }
+
+  //public BufferedImage imageAtTimeAndZ(Integer timeIndex, Integer zIndex) throws IOException, InvalidRangeException {
+
+  //}
+
+  private ConcentrationVarInfo infoForTimeAndZ(int timeIndex, int zIndex, NetcdfFile file) throws InvalidRangeException, IOException {
+    // double concentration(time=8, z=1, y=27, x=36);
+    Variable v = file.findVariable("concentration");
+
+    int xDimensionIndex = v.findDimensionIndex("x");
+    int yDimensionIndex = v.findDimensionIndex("y");
+    Dimension xDimension = v.getDimension(xDimensionIndex);
+    Dimension yDimension = v.getDimension(yDimensionIndex);
+    int[] origin = new int[]{timeIndex, zIndex, 0, 0};
+    int[] size = new int[]{1, 1, yDimension.getLength(), xDimension.getLength()};
+    Array a = v.read(origin, size);
+
+    var d = new ConcentrationVarInfo();
+    d.xDimensionIndex = xDimensionIndex;
+    d.yDimensionIndex = yDimensionIndex;
+    d.xDimension = xDimension;
+    d.yDimension = yDimension;
+    d.array = a;
+    return d;
   }
 }
