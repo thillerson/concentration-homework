572b9be186a594a795f1425be902d03f9ce6aae9
Tony Hillerson <tony.hillerson@gmail.com>
2024-08-31 22:52:15 -0600

    Better organization

--- src/main/java/com/thcontest/exception/IndexOutOfRangeException.java
+++ src/main/java/com/thcontest/exception/IndexOutOfRangeException.java
@@ -1,4 +1,4 @@
-package com.thcontest.response.exception;
+package com.thcontest.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
